e634e0af0d24e86e61373abf6d976497b0fc6f5e
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-02 20:18:55 +0300

    pagination

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,27 +2,35 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 import { useGlobalContext } from '../context';
+import { useState } from 'react';
 
 const Gallery = () => {
+  const [pageNumber, setPageNumber] = useState(1);
+
   const { searchTerm } = useGlobalContext();
 
   const apiKey = import.meta.env.VITE_API_KEY;
   const apiUrl = import.meta.env.VITE_API_URL;
 
-  const URL = `${apiUrl}?client_id=${apiKey}&query=${searchTerm}`;
+  const URL = `${apiUrl}?client_id=${apiKey}&query=${searchTerm}&per_page=12&page=${pageNumber}`;
 
   const { isLoading, isError, data } = useQuery({
-    queryKey: ['images', searchTerm],
+    queryKey: ['images', [searchTerm, pageNumber]],
     queryFn: async () => {
       const res = await axios.get(URL);
       return res.data;
     },
   });
 
+  const prevPage = () => {
+    setPageNumber((prev) => (prev > 1 ? prev - 1 : 1));
+  };
+  const nextPage = () => {
+    setPageNumber((prev) => (prev > total - 1 ? total : prev + 1));
+  };
+
   if (isLoading) {
-    return (
-        <div className="loading"></div>
-    );
+    return <div className='loading'></div>;
   }
 
   if (isError) {
@@ -34,6 +42,7 @@ const Gallery = () => {
   }
 
   const results = data.results;
+  const total = data.total_pages;
 
   if (results.length === 0) {
     return (
@@ -44,11 +53,33 @@ const Gallery = () => {
   }
 
   return (
-    <section className='image-container'>
-      {results.map(({ id, alt_description, urls }) => (
-        <img src={urls.regular} alt={alt_description} key={id} className='image' />
-      ))}
-    </section>
+    <>
+      <section className='image-container'>
+        {results.map(({ id, alt_description, urls, links, user }) => (
+          <a href={links.download} key={id} target='_blank' rel='noreferrer' className='image-link'>
+            <img src={urls.regular} alt={alt_description} className='image' />
+            <p className='image-meta'>{user.name}</p>
+          </a>
+        ))}
+      </section>
+      <div className='button-container'>
+        <button
+          type='button'
+          className='button'
+          onClick={prevPage}
+          style={pageNumber < 2 ? { backgroundColor: 'gray', cursor: 'default' } : null}>
+          prev
+        </button>
+        <span>{pageNumber}</span>
+        <button
+          type='button'
+          className='button'
+          onClick={nextPage}
+          style={pageNumber > total - 1 ? { backgroundColor: 'gray', cursor: 'default' } : null}>
+          next
+        </button>
+      </div>
+    </>
   );
 };
 
