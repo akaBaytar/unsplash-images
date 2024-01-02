f63fd10c9c3ff22557704187a3620c3a45b00388
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-02 14:16:26 +0300

    image rendering

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ const apiKey = import.meta.env.VITE_API_KEY;
 const url = `https://api.unsplash.com/search/photos?client_id=${apiKey}&query=cat`;
 
 const Gallery = () => {
-  const response = useQuery({
+  const { isLoading, isError, data } = useQuery({
     queryKey: ['images'],
     queryFn: async () => {
       const res = await axios.get(url);
@@ -13,7 +13,39 @@ const Gallery = () => {
     },
   });
 
-  return <div>Gallery</div>;
+  if (isLoading) {
+    return (
+      <section className='image-container'>
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  if (isError) {
+    return (
+      <section className='image-container'>
+        <h4>An error occurred.</h4>
+      </section>
+    );
+  }
+
+  const results = data.results;
+
+  if (results.length === 0) {
+    return (
+      <section className='image-container'>
+        <h4>No results found.</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className='image-container'>
+      {results.map(({ id, alt_description, urls }) => (
+        <img src={urls.regular} alt={alt_description} key={id} className='image'/>
+      ))}
+    </section>
+  );
 };
 
 export default Gallery;
