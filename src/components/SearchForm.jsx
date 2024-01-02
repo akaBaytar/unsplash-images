2193572b407b7530abb4abb4769b578746a81300
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-02 13:29:20 +0300

    search form setup

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,21 @@
 const SearchForm = () => {
-  return (
-    <div>SearchForm</div>
-  )
-}
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return
+  };
 
-export default SearchForm
+  return (
+    <section>
+      <h1 className='title'>Unsplash Images</h1>
+      <form className='search-form' onSubmit={submitHandler}>
+        <input type='text' name='search' placeholder='cat' className='form-input search-input' />
+        <button type='submit' className='button'>
+          Search
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
